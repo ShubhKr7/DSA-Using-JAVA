c37977b705f59bb7eb17e72e1d10d4bdf8308513
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2024-09-01 22:42:20 +0530

    changes done

--- leetcodeQuestions/reversePolish.java
+++ leetcodeQuestions/reversePolish.java
@@ -3,13 +3,18 @@ import java.util.*;
 
 public class reversePolish {
 
+    //To solve the given polish notation
     public static int solve(String tokens[]){
 
+        //Stack to store elements
         Stack<Integer> st=new Stack<>();
         for(String val:tokens){
             if(val!="+"&&val!="-"&&val!="*"&&val!="/"){
-            st.push(Integer.parseInt(val)); continue;}
+            st.push(Integer.parseInt(val));
+            //if number inserted then continue to next value
+            continue;}
             
+            //calculating the given operation
             int digit1=st.pop();
             int digit2=st.pop();
             int cal=0;
@@ -17,13 +22,22 @@ public class reversePolish {
             else if(val=="-") cal=digit2-digit1;
             else if(val=="*") cal=digit1*digit2;
             else if(val=="/") cal=digit2/digit1;
+            //Pushing answer into the stack
             st.push(cal);
         }
+        //Returning the final result
         return st.pop();
     }
+
+    //Main function
     public static void main(String[] args) {
+        //Making an array of strings
         String tokens[]={"2","1","+","3","*"};
+
+        //SOlving the notation
         int res=solve(tokens);
+
+        //Displaying result
         System.out.println("Result:"+res);
     }
 }
