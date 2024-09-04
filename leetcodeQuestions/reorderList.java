0f4b83e0c4b9cc8329193e46d95f0a3b0fc60773
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2024-09-04 11:51:05 +0530

    question added on reorder list question

--- leetcodeQuestions/reorderList.java
+++ leetcodeQuestions/reorderList.java
@@ -1,6 +1,25 @@
+/*
+ 143. Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ */
+
 package leetcodeQuestions;
 import java.util.*;
-
 public class reorderList{
 
     //Creating class for ListNode
@@ -16,11 +35,17 @@ public class reorderList{
     public void reOrder(ListNode head){
         ListNode p=head;
         ArrayList<Integer> arr=new ArrayList<>();
+
+        //Adding elements of linked list into an array
         while(p!=null){
             arr.add(p.val);
             p=p.next;
         }
+
+        //Taking the last index of arraylist to iterate from right as well
         int z=arr.size()-1;
+
+        //Start iterating from left
         for(int i=0;i<=z;i+=1){
 
             //Inserting elements from left end
