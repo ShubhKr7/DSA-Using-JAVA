1a6c0ecffd793a817e3f329a00a8d9c4592088bd
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2024-09-06 06:49:38 +0530

    Subset question comments added

--- leetcodeQuestions/subsetsII.java
+++ leetcodeQuestions/subsetsII.java
@@ -21,22 +21,38 @@ public class subsetsII {
     
     //Function to calculate result
     public static List<List<Integer>> subsets(int []nums){
+
+        //ArrayList to store result
          List<List<Integer>> res=new ArrayList<>();
+
+         //Calling recursive function
          recur(res,new ArrayList<>(), nums,0);
 
          return res;
     }
 
+    //Recursive Function
     public static void recur(List<List<Integer>> res, ArrayList<Integer>list, int [] nums, int k){
+
+        //If list already does not exist then add list to res
         if(!res.contains(list))
         res.add(new ArrayList<>(list));
+
+        //Iterate for each element from the kth index
         for(int i=k; i<nums.length; i+=1){
+
+            //Add element to list
             list.add(nums[i]);
+
+            //Recursive call
             recur(res,list,nums,i+1);
+
+            //Remove element while backtracking
             list.remove(list.size()-1);
         }
     }
 
+    //Main Function
     public static void main(String[] args) {
         int nums[]={1,2,2};
         List<List<Integer>> res=subsets(nums);
