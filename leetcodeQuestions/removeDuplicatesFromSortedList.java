62193840d9baff9148c41e76872d9aa23669e1e8
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2025-03-04 09:32:50 +0530

    code updated

--- leetcodeQuestions/removeDuplicatesFromSortedList.java
+++ leetcodeQuestions/removeDuplicatesFromSortedList.java
@@ -17,7 +17,6 @@ The number of nodes in the list is in the range [0, 300].
 -100 <= Node.val <= 100
 The list is guaranteed to be sorted in ascending order.
  */
-
 package leetcodeQuestions;
 import java.util.TreeSet;
 
